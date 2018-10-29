816c77368f0fa4c4bab6a63ce3fef0a1d5751279
ninpnin <vaino.yrjanainen@aalto.fi>
2018-10-29 17:39:21 +0200

    Implement basic neural network

--- classify.py
+++ classify.py
@@ -1,3 +1,65 @@
+from keras.models import Sequential
+from keras.layers import Dense
 import numpy as np
 
-data = np.random.randn(30, 30)
+from numpy import genfromtxt
+
+
+trainsize = 3500
+
+inputs = 264
+genres = 10
+
+X = genfromtxt('train_data.csv', delimiter=',')
+
+Y_0 = genfromtxt('train_labels.csv', delimiter=',')
+
+print(X.shape)
+
+Y = np.zeros((Y_0.shape[0], genres))
+
+X = X / np.mean(X, axis=0)
+
+for i in range(Y_0.shape[0]):
+	index = int(Y_0[i]) - 1
+	Y[i][index] = 1.0
+
+print(X)
+print("Y before", Y)
+print(Y.shape)
+
+ind = np.arange(X.shape[0])
+print(ind)
+np.random.shuffle(ind)
+print(X.shape)
+X = X[ind]
+Y = Y[ind]
+X.shape
+print(ind)
+print("Y after", Y)
+
+# create model
+model = Sequential()
+model.add(Dense(30, input_dim=inputs, activation='relu'))
+model.add(Dense(12, activation='relu'))
+model.add(Dense(12, activation='relu'))
+model.add(Dense(12, activation='relu'))
+model.add(Dense(7, activation='relu'))
+model.add(Dense(genres, activation='softmax'))
+
+
+# Compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+X0 = X[:trainsize]
+Y0 = Y[:trainsize]
+
+X1 = X[trainsize:]
+Y1 = Y[trainsize:]
+
+
+model.fit(X0, Y0, epochs=50, batch_size=10)
+
+# evaluate the model
+scores = model.evaluate(X1, Y1)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
