0097fbd8330881c34dbc747bfc40018faa5e218e
ninpnin <vaino.yrjanainen@aalto.fi>
2018-10-30 12:00:37 +0200

    Update network structure

--- binary.py
+++ binary.py
@@ -8,31 +8,26 @@ from numpy import genfromtxt
 
 trainsize = 3300
 
-inputs = 50
-#inputs = 264
-genres = 10
+inputs = 264
+genres = 1
 
-X = genfromtxt('data/pca_50.csv', delimiter=',')
-#X = genfromtxt('train_data.csv', delimiter=',')
+X = genfromtxt('train_data.csv', delimiter=',')
 
 Y_0 = genfromtxt('train_labels.csv', delimiter=',')
 
 print(X.shape)
 
-Y = np.zeros((Y_0.shape[0], genres))
+Y = np.zeros(Y_0.shape[0])
 
-X = (X - np.mean(X, axis=0) ) / (np.std(X, axis=0) + 0.001* abs(np.mean(X, axis=0)))
+#X += np.random.rand(X.shape[0], X.shape[1])*0.000000000001
+X = (X- np.min(X, axis=0)) / (np.max(X, axis=0)-np.min(X, axis=0)+0.0000001)
 
-
-numbers = np.arange(genres)
+print(X)
 
 for i in range(Y_0.shape[0]):
 	index = int(Y_0[i]) - 1
-	numbers[index] += 1
-	Y[i][index] = 1.0
-
-print("CLASS ", numbers)
-print("CLASS ", numbers / np.sum(numbers))
+	if index == 0:
+		Y[i] = 1.0
 
 print(X)
 print("Y before", Y)
@@ -51,15 +46,17 @@ print("Y after", Y)
 # create model
 model = Sequential()
 model.add(Dense(30, input_dim=inputs, activation='relu', bias_initializer='ones'))
-model.add(Dropout(0.7))
-model.add(Dense(6, activation='relu', bias_initializer='ones'))
-model.add(Dense(5, activation='relu'))
-model.add(Dense(5, activation='relu', bias_initializer='ones'))
-model.add(Dense(genres, activation='softmax'))
+model.add(Dropout(0.2))
+model.add(Dense(4, activation='relu', bias_initializer='ones'))
+model.add(Dropout(0.2))
+model.add(Dense(3, activation='relu', bias_initializer='ones'))
+model.add(Dropout(0.1))
+model.add(Dense(3, activation='relu', bias_initializer='ones'))
+model.add(Dense(genres, activation='sigmoid'))
 
 
 # Compile model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 X0 = X[:trainsize]
 Y0 = Y[:trainsize]
